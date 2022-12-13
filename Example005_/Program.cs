dedd4149fea713739736d0ce72d7293d9c7a00c2
Ivan Tereshchnko <geminimzk@gmail.com>
2022-12-14 00:08:43 +0400

    added Example 005

---
+++ Example005_/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Write your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+   Console.WriteLine("You are lucky Masha");     
+}
+ else
+{
+    Console.Write ("You are not lucky ");
+    Console.Write(username);
+ }
