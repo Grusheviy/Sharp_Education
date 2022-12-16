abe4b069fc7df5bf5b4e98981475ea9965120e7d
Ivan Tereshchnko <geminimzk@gmail.com>
2022-12-16 13:44:21 +0400

    End Task 8

--- Sharp_seminar_homework/HomeWork/Task_8/Program.cs
+++ Sharp_seminar_homework/HomeWork/Task_8/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+//C отсчетом от нуля, если нужно как у Вас в примере то i=1
+Console.Write("write number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+    for(int i=0; i <= n; i++)
+    {
+        if (i % 2 == 0)
+            {
+            Console.Write( i + " ");
+     
+            }
+    }
