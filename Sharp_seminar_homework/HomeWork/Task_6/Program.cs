85ddca5de834e27e803919c9b201561b7ed91672
Ivan Tereshchnko <geminimzk@gmail.com>
2022-12-16 13:29:04 +0400

    End Task 6

---
+++ Sharp_seminar_homework/HomeWork/Task_6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Write number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.Write( a + " is even number");
+}
+else
+{
+    Console.Write( a + " is odd number");
+}
