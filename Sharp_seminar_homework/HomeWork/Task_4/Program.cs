041f4909cb1d167e8f001ce99d4a7a7b204e4a14
Ivan Tereshchnko <geminimzk@gmail.com>
2022-12-16 13:17:02 +0400

    end Task_4

--- Sharp_seminar_homework/HomeWork/Task_4/Program.cs
+++ Sharp_seminar_homework/HomeWork/Task_4/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Write first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Write second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Write third number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if(b > max)
+{
+    max = b;
+}
+if(c > max)
+{
+    max = c;
+}
+Console.Write("Max number = " + max);
