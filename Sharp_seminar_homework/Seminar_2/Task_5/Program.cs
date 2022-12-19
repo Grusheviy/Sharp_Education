8eb7a277f9ed908eaf2f88eea269c5fe9de2803b
Ivan Tereshchnko <geminimzk@gmail.com>
2022-12-19 22:55:09 +0400

    End of Seminar_2 added Tasks 1, 2, 3, 4, 5

---
+++ Sharp_seminar_homework/Seminar_2/Task_5/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+// 5, 25  ->  да
+// -4, 16  ->  да
+// 25, 5  ->  да
+// 8,9  ->  нет
+
+Console.Write("Vvedite chislo 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Vvedite chislo 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a = b * b || b = a * a)
+{
+Console.Write("Da");
+}
+else
+{
+Console.Write("Net");
+}
