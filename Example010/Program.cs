ac7b5cb4accbd4256e5e944a2b8af14b13660eb5
Ivan Tereshchnko <geminimzk@gmail.com>
2023-01-10 21:26:17 +0400

    added example010

---
+++ Example010/Program.cs
@@ -0,0 +1,73 @@
+﻿// void Method1()
+// {
+//     Console.Write("avtor");
+// }
+// Method2();
+
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// Method2 ("text");
+
+// void Method3 (string msg, int count)
+// {
+//     int i = 0;
+//     while (i<ocunt)
+//     {
+//         Console.WriteLine(msg);
+//         count++;
+//     }
+// }
+// Method3(msg: "text", 4);
+
+// Method3(count: 4, msg: "new text");
+
+// void Method4()
+// {
+//     return DateTime.Now.Year;
+// }
+// int year = Method4();
+// Console.WriteLine(year);
+
+// void Method5(int count, string text);
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//          result = result + text;
+//     }
+//     return result;
+// }
+
+// string res = Method5(10, "qwerty");
+// Console.WriteLine(res);
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} * {j} = {i*j}");
+//     }
+//     Console.WriteLine();
+// }
+
+string text = "Я думаю, - сказал князь, улыбаясь,  - что,"
+            + "если бы вас послали вместо нашего милорда";
+
+string Replase(string text, char oldValue, char newValue)
+{
+string result = String.Empty;
+int length = text.Length;
+
+    for (int i = 0; i < length; i++)
+    {
+    if(text[i] = oldValue, result + $"{NewValue}");
+    else result + $"{str[i]}";
+    }
+return result;
+}
+string newText = replace(text,' ', '|');
+Console.WriteLine(newText);
+string newText = replace(text,'к', 'ф');
+Console.WriteLine(newText);
