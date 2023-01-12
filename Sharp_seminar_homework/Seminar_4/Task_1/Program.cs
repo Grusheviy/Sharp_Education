f7ca63768d0423cfa5f08f18356906862d6c73d0
Ivan Tereshchnko <geminimzk@gmail.com>
2023-01-12 23:00:11 +0400

    added Task_1

---
+++ Sharp_seminar_homework/Seminar_4/Task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа принимает на вход число А и выдает сумму от 1 до А
+// 4 -> 10
+// 8 -> 36
+
+int Sum(int number)
+{
+    int sum = 0;
+    for(int i = 1; i <= number; i++)
+    {
+        sum += i; // sum = sum + i
+    }
+    return sum;
+}
+
+Console.WriteLine("vvedite chislo: ");
+int A = Convert.ToInt32(Console.ReadLine());
+// int sum = Sum(A);
+Console.WriteLine(Sum(A));
+
+
