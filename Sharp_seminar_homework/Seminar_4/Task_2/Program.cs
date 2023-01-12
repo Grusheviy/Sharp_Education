2a960cfd01adaa6d748a1231d38bb04a0d649f09
Ivan Tereshchnko <geminimzk@gmail.com>
2023-01-12 23:00:01 +0400

    added Task_2

---
+++ Sharp_seminar_homework/Seminar_4/Task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿
+// int  Count(int number)
+// {
+//     int count;
+//     for (count = 0; number > 0; count ++)
+//     {
+//         number /= 10;
+//     }
+//     return count;
+// }
+
+// Console.WriteLine("Write your number: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int count = Count(N);
+// Console.WriteLine($"your number lenght: {count}");
+
+// Намного проще 
+
+
+Console.WriteLine("Write your number: ");
+string a = Console.ReadLine(); 
+if(int.TryParse(a, out int A)) {
+    Console.WriteLine(a.Length); 
+} else {
+    Console.WriteLine("Error");
+}
