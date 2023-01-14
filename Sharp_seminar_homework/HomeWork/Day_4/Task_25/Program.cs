9d1818f3e1d0719cc1b73d2fa641c275cb441e37
Ivan Tereshchnko <geminimzk@gmail.com>
2023-01-14 12:26:10 +0400

    addad task_25

---
+++ Sharp_seminar_homework/HomeWork/Day_4/Task_25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. 
+// (без использования Math.Pow)
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int MyPow(int A, int B)
+{
+     int result = A;
+    for(int count = 1; count < B; count++)
+    {
+        result *= A;
+    }
+    return result;
+}
+Console.WriteLine("Write you number: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Write you Pow: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int pow = MyPow(A,B);
+Console.Write($"Yor number {A} in Pow {B}: {pow}");
+
+
+
+
