9d1bb96d454b8e0fea7890296e16752227c052b2
Ivan Tereshchnko <geminimzk@gmail.com>
2023-01-14 13:40:44 +0400

    Added Task_27

---
+++ Sharp_seminar_homework/HomeWork/Day_4/Task_27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int  Count(int number)
+{
+    int count;
+    int Sum = 0;    
+    for (count = 0; number > 0; count ++)
+    {
+    Sum += number % 10;
+    number /=10;
+    }
+     return Sum;
+}
+Console.WriteLine("Write your number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = Count(n);
+Console.WriteLine($"your number Sum: {count}");  
