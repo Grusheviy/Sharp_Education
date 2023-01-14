78c40af741bc96123d75b63fc941835b4acc29eb
Ivan Tereshchnko <geminimzk@gmail.com>
2023-01-14 14:34:58 +0400

    Added Task_29

---
+++ Sharp_seminar_homework/HomeWork/Day_4/Task_29/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void FillArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0,99);
+}
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+       Console.Write($"[ "+(array[i])+ " ]");
+    }
+    Console.WriteLine();
+}
+
+
+Console.WriteLine("Write you array length: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+FillArray(array);
+PrintArray(array); 
