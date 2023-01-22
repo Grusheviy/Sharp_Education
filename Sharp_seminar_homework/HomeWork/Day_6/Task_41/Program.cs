b597df3720e29000ed1716729a353b97a7c7e38c
Ivan Tereshchnko <geminimzk@gmail.com>
2023-01-22 11:44:20 +0400

    Added Task_41

---
+++ Sharp_seminar_homework/HomeWork/Day_6/Task_41/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Write your numbers: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void CountPositiv(int[] array)
+{
+int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if (array[i] > 0)
+    {
+        count++;
+    }
+    }
+Console.WriteLine($"Кол-во элементов > 0: {count}");
+}
+
+Console.Write("Write your array length: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [length];
+Console.Write("You array: ");
+Console.WriteLine(string.Join(", ", array));
+FillArray(array);
+CountPositiv(array);
+
+
