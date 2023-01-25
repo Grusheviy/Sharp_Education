687b4ee6fa18f27ea2d4e9536e98161dbe86b6e2
Ivan Tereshchnko <geminimzk@gmail.com>
2023-01-25 12:58:26 +0400

    Added Day_7 HomeWork and ended Task_47

---
+++ Sharp_seminar_homework/HomeWork/Day_7/Task_47/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+void FillArrayWithRandom(double[,] array, int row, int column)
+{
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < column; j++)
+        {
+            array[i,j] = new Random().NextDouble()* 10;
+        }
+        
+    }
+}
+void PrintArrayWithRandom(double[,] array, int m, int n)
+{
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            Console.Write($"[ {Math.Round(array[i,j], 1)} ] ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("write m:");
+int m = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("write n:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double [,] array = new double[m,n];
+
+FillArrayWithRandom(array, m, n);
+PrintArrayWithRandom(array, m, n);
