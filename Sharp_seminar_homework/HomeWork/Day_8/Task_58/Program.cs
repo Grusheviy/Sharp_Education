5d3b885de01281f96da14911522a3e9ed32bbb6c
Ivan Tereshchnko <geminimzk@gmail.com>
2023-01-28 17:16:13 +0400

    Added Task_60

---
+++ Sharp_seminar_homework/HomeWork/Day_8/Task_58/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+
+
+
+
+
+
+Console.WriteLine("write your  m:");
+int m = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("write your n:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("write your b:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int[m,n];
---
+++ Sharp_seminar_homework/HomeWork/Day_8/Task_60/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+void FillArrayWithRandom(int[,,] matrix)
+{
+  int[] temp = new int[matrix.GetLength(0) * matrix.GetLength(1) * matrix.GetLength(2)];
+  int number;
+  for(int i = 0; i < temp.GetLength(0); i++)
+    {
+    temp[i] = new Random().Next(10, 100);
+    number = temp[i];
+    if(i >= 1)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            if(temp[i] == temp[j])
+            {
+            temp[i] = new Random().Next(10, 100);
+            j = 0;
+            number = temp[i];
+            }
+          number = temp[i];
+        }
+    }
+    }
+  int count = 0; 
+  for(int x = 0; x < matrix.GetLength(0); x++)
+  {
+    for(int y = 0; y < matrix.GetLength(1); y++)
+    {
+      for(int z = 0; z < matrix.GetLength(2); z++)
+      {
+        matrix[x, y, z] = temp[count];
+        count++;
+      }
+    }
+  }
+}
+void PrintArrayWithRandom(int[,,] matrix)
+{
+  for(int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+      for(int k = 0; k < matrix.GetLength(2); k++)
+      {
+        Console.Write( $"{matrix[i,j,k]}({k},{i},{j}); ");
+      }
+      Console.WriteLine();
+    }
+    Console.WriteLine();
+  }
+}
+
+Console.WriteLine($"Write matrix size X x Y x Z:");
+Console.WriteLine("Write your  X:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Write your  Y:");
+int y = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Write your  Z:");
+int z = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine();
+
+int[,,] matrix = new int[x, y, z];
+
+FillArrayWithRandom(matrix);
+PrintArrayWithRandom(matrix);
