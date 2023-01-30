d229abcda5ec822ff1d83698cdf21b4b98eb3db8
Ivan Tereshchnko <geminimzk@gmail.com>
2023-01-30 22:36:10 +0400

    Added all tasks from Seminar_9_Last

---
+++ Sharp_seminar_homework/Seminar_9_Last/Task_4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B 
+// с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+
+
+int POW(int n, int m)
+{
+    int exp = 1;
+    if(m == 0)
+    {
+        return exp;
+    }
+    return exp = n * POW(n, m - 1);
+}
+
+Console.Write("Write number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Write POW: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int exp = POW(m, n);
+
+Console.WriteLine(exp);
